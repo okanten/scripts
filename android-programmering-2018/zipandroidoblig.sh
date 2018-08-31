b430d262ea11e0c0aa365414b0196ce6d3421b7a
O.K <okgit@protonmail.com>
2018-08-31 14:30:19 +0200

    Added a line to copy app-debug.apk to the root of the project-folder if it doesnt exist.

--- android-programmering-2018/zipandroidoblig.sh
+++ android-programmering-2018/zipandroidoblig.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 fullname="YourNameHere" # Put your name here without spaces (e.g.: JohnDoe)
 
-display_usage() { 
-	echo "You need to specify a folder" 
-	echo -e "\nUsage: ./${0##*/} /path/to/folder \n" 
-} 
+display_usage() {
+	echo "You need to specify a folder"
+	echo -e "\nUsage: ./${0##*/} /path/to/folder \n"
+}
 
-if [  $# -le 0 ] 
-then 
+if [  $# -le 0 ]
+then
 	display_usage
 	exit 1
-fi 
+fi
 
 basename=$(basename $1)
 rm -rf /tmp/$basename/
 cp -R $1 /tmp/$basename/
+mv -n /tmp/$basename/app/build/outputs/apk/debug/app-debug.apk /tmp/$basename/app-debug.apk # Flytter apk-filen hvis den ikke eksisterer.
 rm -r /tmp/$basename/.gradle
 rm -r /tmp/$basename/.git
 rm -r /tmp/$basename/app/build
