cde6afa7e7f2aa9a6b07f9c5c98164ac72219e7d
Ola Kanten <31588139+okstudies@users.noreply.github.com>
2018-08-31 15:32:01 +0200

    Fixed the directory tree in the zip file.
    Originally the script zipped /tmp/<foldername> which includes an unneccessary folder. This is now fixed.

--- android-programmering-2018/zipandroidoblig.sh
+++ android-programmering-2018/zipandroidoblig.sh
@@ -1,21 +1,22 @@
 #! /bin/bash
 fullname="YourNameHere" # Put your name here without spaces (e.g.: JohnDoe)
 
-display_usage() { 
-	echo "You need to specify a folder" 
-	echo -e "\nUsage: ./${0##*/} /path/to/folder \n" 
-} 
+display_usage() {
+	echo "You need to specify a folder"
+	echo -e "\nUsage: ./${0##*/} /path/to/folder \n"
+}
 
-if [  $# -le 0 ] 
-then 
+if [  $# -le 0 ]
+then
 	display_usage
 	exit 1
-fi 
+fi
 
 basename=$(basename $1)
+rundir=$(pwd) # Extracts current working directory
 rm -rf /tmp/$basename/
 cp -R $1 /tmp/$basename/
-mv -n /tmp/$basename/app/build/outputs/apk/debug/app-debug.apk /tmp/$basename/app-debug.apk # Flytter apk-filen hvis den ikke eksisterer.
+mv -n /tmp/$basename/app/build/outputs/apk/debug/app-debug.apk /tmp/$basename/app-debug.apk # Moves the app-debug.apk if it hasnt already been moved
 rm -r /tmp/$basename/.gradle
 rm -r /tmp/$basename/.git
 rm -r /tmp/$basename/app/build
@@ -24,4 +25,6 @@ rm -r /tmp/$basename/captures
 rm -r /tmp/$basename/projectFilesBackup
 rm /tmp/$basename/.idea/vcs.xml
 rm /tmp/$basename/local.properties
-zip -r "${basename}_Android_${fullname}.zip" /tmp/$basename/
+pushd /tmp # Jump into /tmp so the directory tree in the .zip file doesnt include /tmp
+zip -r "$rundir/${basename}_Android_${fullname}.zip" $basename/
+exit 0
