29d91027c3adeaa60247638633dbd8aa6ad34715
O.K <okgit@protonmail.com>
2018-08-31 13:33:31 +0200

    Initial commit

---
+++ android-programmering-2018/README.md
@@ -0,0 +1,1 @@
+Et lite bash script som fjerner overflødige filer før den zipper mappen klar for innlevering.
---
+++ android-programmering-2018/zipandroidoblig.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+fullname="YourNameHere" # Put your name here without spaces (e.g.: JohnDoe)
+
+display_usage() { 
+	echo "You need to specify a folder" 
+	echo -e "\nUsage: ./${0##*/} /path/to/folder \n" 
+} 
+
+if [  $# -le 0 ] 
+then 
+	display_usage
+	exit 1
+fi 
+
+basename=$(basename $1)
+rm -rf /tmp/$basename/
+cp -R $1 /tmp/$basename/
+rm -r /tmp/$basename/.gradle
+rm -r /tmp/$basename/.git
+rm -r /tmp/$basename/app/build
+rm -r /tmp/$basename/build
+rm -r /tmp/$basename/captures
+rm -r /tmp/$basename/projectFilesBackup
+rm /tmp/$basename/.idea/vcs.xml
+rm /tmp/$basename/local.properties
+zip -r "${basename}_Android_${fullname}.zip" /tmp/$basename/
